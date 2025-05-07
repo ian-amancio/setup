510998e21d62e98b510416def44d2effec72c8e7
Ian <ianamancio203@gmail.com>
2025-05-07 17:45:00 -0300

    Update: awesome theme

--- system_files/config/awesome/theme/theme.lua
+++ system_files/config/awesome/theme/theme.lua
@@ -1,33 +1,30 @@
 local theme_assets                              = require("beautiful.theme_assets")
 local xresources                                = require("beautiful.xresources")
-local dpi                                       = xresources.apply_dpi
-
 local gears                                     = require("gears")
+
+local dpi                                       = xresources.apply_dpi
 local themes_path                               = gears.filesystem.get_configuration_dir() .. "theme/"
 local default_theme_path                        = gears.filesystem.get_themes_dir() .. "default/"
-
--- local gfs                                       = require("gears.filesystem")
--- local themes_path                               = gfs.get_themes_dir()
-
 local theme                                     = {}
 
 theme.font                                      = "hack 8"
+theme.wallpaper                                 = themes_path .. "bg.jpg"
 
 theme.bg_normal                                 = "#222222"
-theme.bg_focus                                  = "#535d6c"
+theme.bg_focus                                  = "#309898"
 theme.bg_urgent                                 = "#ff0000"
 theme.bg_minimize                               = "#444444"
 theme.bg_systray                                = theme.bg_normal
 
 theme.fg_normal                                 = "#aaaaaa"
-theme.fg_focus                                  = "#ffffff"
+theme.fg_focus                                  = "#000000"
 theme.fg_urgent                                 = "#ffffff"
 theme.fg_minimize                               = "#ffffff"
 
 theme.useless_gap                               = dpi(0)
 theme.border_width                              = dpi(1)
-theme.border_normal                             = "#000000"
-theme.border_focus                              = "#535d6c"
+theme.border_normal                             = theme.bg_normal
+theme.border_focus                              = theme.bg_focus
 theme.border_marked                             = "#91231c"
 
 -- There are other variable sets
@@ -57,8 +54,6 @@ theme.taglist_squares_unsel                     = theme_assets.taglist_squares_u
 -- notification_[bg|fg]
 -- notification_[width|height|margin]
 -- notification_[border_color|border_width|shape|opacity]
-
-theme.wallpaper                                 = themes_path .. "bg.jpg"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
